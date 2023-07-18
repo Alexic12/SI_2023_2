f6fe9d5b90ef10e8db72d8023cca8d9f23fee250
Camilo-Diez <camilo.diez@eia.edu.co>
2023-07-18 10:01:05 -0500

    COMMIT_DATA

---
+++ hubs/data_hub.py
@@ -0,0 +1,64 @@
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+
+import sklearn.preprocessing
+import sklearn.model_selection
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split as tts
+
+class Data:
+    """
+    Attributes:
+
+    Methods:
+
+    """
+
+    def __init__(self):
+        pass
+
+    def data_process(self,file):
+        ##Let's define the absolute path for this folder 
+        data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','data'))
+
+        ##Find the complete excel file route
+        excel_path = os.path.join(data_dir, file)
+
+        ##Loads the raw excel file 
+        data_raw =  pd.read_excel(excel_path, sheet_name = 0)
+
+        ##Let's convert the raw data to an array
+        data_arr = np.array(data_raw)
+
+        ##Let's label encode any text in the data
+
+        #Let's create a boolean array with size of the columns
+        str_cols = np.empty(data_arr.shape[1], dtype = bool)
+
+        #Let's read columns data type
+        for i in range (0,data_arr.shape[1]):
+            str_cols[i] = np.issubdtype(type(data_arr[0,i]), np.str_)
+
+        for i in range (0,data_arr.shape[1]):
+            if str_cols[i]:
+                le = LabelEncoder()
+                data_arr[:,i] = le.fit_transform(data_arr[:,i]) + 1
+
+        ##Let's split the data into features (inputs) and labels(outputs)
+        data_features = data_arr[:,0:-1] ## all columns but the last
+        data_labels = data_arr[:,-1] ## The last column        
+
+        ##Let's normalize the data
+        scaler = StandardScaler() ##Create an object of this library in particular
+
+        data_features_norm = scaler.fit_transform(data_features)
+        data_labels_norm = scaler.fit_transform(data_labels)
+
+        print(data_labels_norm)
+
+
+    
