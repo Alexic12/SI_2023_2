441dcda1f27bf1d78317dcb3dbc473e47fadcfaf
ManuelOrtizCamacho <manuel.ortiz@eia.edu.co>
2023-07-18 09:58:27 -0500

    COMMIT_DATA

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#sadadsadsadsa
+from hubs.data_hub import Data # esto se hace en la industria 
+
+
+data = Data() # crear un objeto pra los atributos  tenemos datos numericos, tambien puede ser string entonces toca transformar a datos , toca codificar los string en datos 
+
+Data.data_process('ETHEREUM_PRICE.xlsx')
+
+
+
