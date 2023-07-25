e0e6e11d660ed349c7baaa8fd560899c56dccca3
AlexanderAntolinez <alexander201195@gmail.com>
2023-07-25 13:32:19 -0500

    commit25072023

--- hubs/data_hub.py
+++ hubs/data_hub.py
@@ -20,45 +20,51 @@ class Data:
 
     """""
 
-def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-def data_process (self,file): 
-    ##Lets define the absolute pat for this folder
-    data_dir= os.path.abspath(os.path.join(os.path.dirname(__file__),"..","data"))
+    def data_process (self,file): 
+        ##Lets define the absolute pat for this folder
+        data_dir= os.path.abspath(os.path.join(os.path.dirname(__file__),"..","data"))
 
-    ##Find the complete excel file route
-    excel_path = os.path.join(data_dir,file)
+        ##Find the complete excel file route
+        excel_path = os.path.join(data_dir,file)
 
-    ##lets load the raw excel file
-    data_raw= pd.read_excel(excel_path, sheet__name = 0)
+        ##lets load the raw excel file
+        data_raw= pd.read_excel(excel_path, sheet_name = 0)
 
-    ##Lets convert the raw data to an array
-    data_arr= np.array(data_raw)
+        ##Lets convert the raw data to an array
+        data_arr= np.array(data_raw)
 
-    ##Lets label encode any text in the data
+        ##Lets label encode any text in the data
 
 
-    ##Lets create a boolean array with the size of de colum
-    str_cols = np.empty(data_arr.shape[1], dtype= bool)
+        ##Lets create a boolean array with the size of de colum
+        str_cols = np.empty(data_arr.shape[1], dtype= bool)
 
-    ##Lets read columns data type
-    for i in range(0,data_arr.shape[1]):
-        str_cols[i] = np.issubdtype(type(data_arr[0,i],np.str_))
-    
-    for i in range(0,data_arr.shape[1]):
-        if str_cols[i]:
-            le = LabelEncoder()
-            data_arr[:,i] = le.fit_transform(data_arr[:,i]) + 1 
+        ##Lets read columns data type
+        for i in range(0,data_arr.shape[1]):
+            str_cols[i] = np.issubdtype(type(data_arr[0,i],np.str_))
 
-    ##Lets split the data into features and labels
-    data_features = data_arr[:,0:-1]##la ultima columna es lo que queremos que aprenda ent se toman todas lascolumnas menos la ultima
-    data_labels = data_arr[:,-1]
-    
-    ##Lets normalize the data 
-    scaler = StandardScaler()##Creante an object of this library in particular 
+        for i in range(0,data_arr.shape[1]):
+            if str_cols[i]:
+                le = LabelEncoder()
+                data_arr[:,i] = le.fit_transform(data_arr[:,i]) + 1 
 
-    data_features_norm = scaler.fit_transform(data_features)
-    data_labels_norm = scaler.fit_transform(data_labels)
+        ##Lets split the data into features and labels
+        data_features = data_arr[:,0:-1]##la ultima columna es lo que queremos que aprenda ent se toman todas lascolumnas menos la ultima
+        data_labels = data_arr[:,-1]
 
-    print(data_labels_norm)
+        data_labels = data_labels.reshape(-1,1)
+
+        ##Lets normalize the data 
+        scaler = StandardScaler()##Creante an object of this library in particular 
+
+        data_features_norm = scaler.fit_transform(data_features)
+        data_labels_norm = scaler.fit_transform(data_labels)
+
+        #print(data_labels_norm)
+
+        ## lets split the data into training and testing
+        train_features, test_features, train_labels, test_labels = tts(data_features_norm,data_labels_norm, test_size = 0.1)
+
