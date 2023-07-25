a590f12fb5d5724b38ed7e5687f3ccd3cd76c8b9
alejojarah <alejojaramilloh2000@gmail.com>
2023-07-25 09:15:28 -0500

    commit_neural

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from hubs.data_hub import data
+"""from hubs.data_hub import data
 
 Data = data()
 
-Data.data_process('ETHEREUM_PRICE.xlsx')
+Data.data_process('ETHEREUM_PRICE.xlsx')"""
+
+from hubs.neural_hub import Neural
+
+N = Neural
+
+N.run_model(model = 'perceptron', file_name = 'ETHEREUM_PRICE.xlsx')
