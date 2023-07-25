448213da0519205d7b36712f47c66a94b142b6be
PaezF <manuel.paez@eia.edu.co>
2023-07-25 09:15:37 -0500

    COMMIT_NEURAL

---
+++ hubs/models/perceptron.py
@@ -0,0 +1,11 @@
+
+
+class Perceptron:
+    def __init__(self) -> None:
+        pass
+
+    def run (self, train_features, test_features, train_labels, test_labels):
+        print('Training perceptron netwwork.....')
+        ##here is where all the neural network code is gonna be
+
+        ##training and testing is done here
