f10ce91cb8699f8af9f3663aa6ff003538186f59
Jhon-Marin <Jhon-Marin>
2023-08-03 08:16:02 -0500

    COMMIT_NEURAL

---
+++ hubs/models/perceptron.py
@@ -0,0 +1,12 @@
+
+
+class Perceptron:
+    def __init__(self):
+        pass
+
+    def run(self, train_features, test_features, train_labels, test_labels):
+        print('Training perceptron network...')
+
+        ##Neural network code
+
+        ##Training and testing is done here
---
+++ hubs/neural_hub.py
@@ -0,0 +1,22 @@
+from hubs.data_hub import Data
+from models.perceptron import Perceptron
+
+
+class Neural:
+    def __init__(self):
+        pass
+
+    def run_model(self, model, file_name):
+        data= Data()
+        train_features, test_features, train_labels, test_labels = data.data_process('ETHEREUM_PRICE.xlsx')
+        if model == 'perceptron':
+            print('Running Preceptron Model')
+
+            ##Code for the preceptron model
+            P = Perceptron()
+            P.run(self, train_features, test_features, train_labels, test_labels)
+
+        elif model == 'ffm':
+            print('Running ffm Model')
+            
+            ##Code for the preceptron model
