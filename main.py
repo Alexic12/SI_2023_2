1b787829d467e0da27a593ec071485fc5c6b7454
AlejandraLagosMarquez <AlejandraLagosMarquez>
2023-08-08 09:54:54 -0500

    COMMIT_PERCEPTRON1

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from hubs.neural_hub import Neural
 
 N=Neural()
 #se importa desde el hub, la clase Neural con los parametros(modelo,nombre archivo)
-N.run_model(model='perceptron',file_name='ETHEREUM_PRICE.xlsx')
+N.run_model(model='perceptron',file_name='ETHEREUM_PRICE.xlsx',iter=100)
 
 
