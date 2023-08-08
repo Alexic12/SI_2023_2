40431bd007944107e57fc920e711d906fcd4546e
Carlos Vidal <carlos.vidal.cardona@gmail.com>
2023-08-08 09:53:24 -0500

    COMMIT_PERCEPTRON_FF

--- hubs/neural_hub.py
+++ hubs/neural_hub.py
@@ -1,12 +1,12 @@
 from hubs.data_hub import Data
-from models.perceptron import Perceptron
+from hubs.models.perceptron import Perceptron
 
 
 class Neural:
     def __init__(self):
         pass
 
-    def run_model(self, model, file_name):
+    def run_model(self, model, file_name, iter):
         data= Data()
         train_features, test_features, train_labels, test_labels = data.data_process('ETHEREUM_PRICE.xlsx')
         if model == 'perceptron':
@@ -14,7 +14,7 @@ class Neural:
 
             ##Code for the preceptron model
             P = Perceptron()
-            P.run(self, train_features, test_features, train_labels, test_labels)
+            P.run(train_features, test_features, train_labels, test_labels, iter)
 
         elif model == 'ffm':
             print('Running ffm Model')
