0892f799840f87a22706b032ac6fcf54b1feea00
AlexanderAntolinez <alexander201195@gmail.com>
2023-08-10 09:53:53 -0500

    COMMIT_PERCEPTRON_FULL

--- hubs/models/perceptron.py
+++ hubs/models/perceptron.py
@@ -1,9 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 class Perceptron:
     def __init__(self) -> None:
         pass
 
-    def run (self, train_features, test_features, train_labels, test_labels,iter):
+    def run (self, train_features, test_features, train_labels, test_labels,iter,alfa):
         print('Training perceptron network....')
 
         ##here is where all the neural network code is gonna be
@@ -12,7 +13,7 @@ class Perceptron:
         ## Lets organize the data
         Xi=np.zeros((train_features.shape[1] + 1 ,1)) #input vecotr , el +1 es el bias 
 
-        Wij= np.zeros((train_labels.shape[1],train_features.shape[1])) # wieght matrix
+        Wij= np.zeros((train_labels.shape[1],train_features.shape[1]+1)) # wieght matrix
 
         Aj= np.zeros((train_labels.shape[1],1)) # Agregation vector 
 
@@ -29,13 +30,15 @@ class Perceptron:
         ##Fill the weight matrix before trainning 
         for i in range (0,Wij.shape[0]):
             for j in range(0,Wij.shape[1]):
-                Wij[i][j]= np.random(-1,1)
+                Wij[i][j]= np.random.uniform(-1,1)
+        
         
         for it in range(0,iter):
             for d in range (0,train_features.shape[0]):
+                Xi[0][0] = 1
                 #pass the data inputs to the input vector Xi
-                for i in range(0,train_features.shape[0]):
-                    Xi[i+1][1]= train_features[d][i]
+                for i in range(0,train_features.shape[1]):
+                    Xi[i+1][0]= train_features[d][i]
 
 
                 #lets calculate the agregation for each neuron
@@ -47,7 +50,7 @@ class Perceptron:
                 #lets calculate the output for each iteration
                 for n in range(0,Yk.shape[0]):
                     if Aj[n][0] < 0:
-                        Yk= [n][0] = 0
+                        Yk[n][0] = 0
                     else:
                         Yk[n][0] = 1
 
@@ -57,12 +60,29 @@ class Perceptron:
                 for i in range(0,train_labels.shape[1]):
                     Yd[i][0] = train_labels[d][i]
                 
-                for n in range(0,Ek.shpae[0]):
-                    Ek[n][0] = Yd [n][0]-Yk[n][0]
+                for n in range(0, Yk.shape[0]):
+                    Ek[n][0] = Yd[n][0]-Yk[n][0]
 
                     #lets add the ECM for this data point 
-                    ecm[n][0] = ecm[n][0]+ ((Ek[n][0]^2)/2)
+                    ecm[n][0] = ecm[n][0]+ ((Ek[n][0]**2)/2)
 
+                #wieight training 
+                for n in range(0,Yk.shape[0]):
+                    for w in range(0,Wij.shape[1]):
+                        Wij[n][w] = Wij[n][w] + alfa*Ek[n][0]*Xi[w][0]
+                
+            print(f'Iter: {it}')
+            for n in range(0,Yk.shape[0]):
+                print(f'ECM {n} : {ecm[n][0]}')
+
+            for n in range(0,Yk.shape[0]):
+                ecmT[n][it] = ecm[n][0]
+                ecm[n][0] = 0
+
+        for n in range(0,Yk.shape[0]):
+            plt.figure()
+            plt.plot(ecmT[n][:],'r',label = f'ECM Neurona {n}')
+            plt.show()
 
 ##training and testing is done here
         
