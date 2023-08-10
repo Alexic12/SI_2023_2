e2f08221594d4e14e8dd95fad00b5961dbf7b95e
Carlos Vidal <carlos.vidal.cardona@gmail.com>
2023-08-10 09:55:38 -0500

    COMMIT_PERCEPTRON_FULL

--- hubs/models/perceptron.py
+++ hubs/models/perceptron.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Perceptron:
     def __init__(self):
         pass
 
-    def run(self, train_features, test_features, train_labels, test_labels, iter):
+    def run(self, train_features, test_features, train_labels, test_labels, iter, alfa):
         print('Training perceptron network...')
 
         ##Neural network code
@@ -24,26 +25,26 @@ class Perceptron:
 
         ecmT = np.zeros((train_labels.shape[1], iter)) #ECM total for all the iterations
 
-        ##Fill the wight Matrix before training
+        ##Fill the wieght Matrix before training
         for i in range(0, Wij.shape[0]):
             for j in range(0, Wij.shape[1]):
-                Wij[i][j] = np.random(-1, 1)
+                Wij[i][j] = np.random.uniform(-1, 1)
 
         for it in range(0, iter):
             for d in range(0, train_features.shape[0]):
                 #pass the data inputs to the input vector Xi
-                Xi[0][0] = 1 ##Bias - Valor semilla que siempre es 1
+                Xi[0][0] = 1 ##Bias --- Valor semilla que siempre es 1
                 
                 for i in range(0 ,train_features.shape[1]):
                     Xi[i+1][0] = train_features[d][i]
 
-                ##Lets calculate the agregation  for each neuron - Es un producto punto
+                ##Lets calculate the agregation for each neuron - Es un producto punto
                 for n in range(0, Aj.shape[0]):
                     for n_input in range(0, Xi.shape[0]):
-                        Aj[n][0] = Aj[n][0] + Aj[n_input]*Wij[n][n_input]
+                        Aj[n][0] = Aj[n][0] + Xi[n_input]*Wij[n][n_input]
                 
-                #Lets calculate the output for each neuron 
-                for n in range(0, Yk.sahpe[0]):
+                #Lets calculate the output for each neuron - es una funcion por tramos
+                for n in range(0, Yk.shape[0]):
                     if Aj[n][0] < 0:
                         Yk[n][0] = 0
                     else:
@@ -52,14 +53,32 @@ class Perceptron:
                 #Lets calculate the error for aech neuron
 
                 #Pass train_labels to Yd vector
-                for i in range(0, train_features.shape[1]):
-                    Yd[i][0] = train_features[d][i]
+                for i in range(0, train_labels.shape[1]):
+                    Yd[i][0] = train_labels[d][i]
 
                 for n in range(0, Ek.shape[0]):
                     Ek[n][0] = Yd[n][0] - Yk[n][0]
 
                     ##Lets add the ECM for this data point
-                    ecm[n][0] = ecm[n][0] + ((Ek[n][0]^2)/2)
+                    ecm[n][0] = ecm[n][0] + (((Ek[n][0])**2)/2)
 
-        ##Training and testing is done here
+                #Weigth training
+                for n in range(0, Yk.shape[0]):
+                    for w in range(0, Wij.shape[1]):
+                        Wij[n][w] = Wij[n][w] + alfa*Ek[n][0]*Xi[w][0]
+
+                print(f'Iter: {it}')
+                for n in range(0, Yk.shape[0]):
+                    print(f'ECM {n}: {ecm[n][0]}')
+
+            for n in range(0, Yk.shape[0]):
+                ecmT[n][it] = ecm[n][0]
+                ecm[n][0] = 0
+
+        for n in range(0, Yk.shape[0]):
+            plt.figure()
+            plt.plot(ecmT[n][:], 'r', label = f'ECM Neurona {n}')
+            plt.show()
+
+        ##Testing is done here
         #print(f'Train features :{train_features}')
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from hubs.neural_hub import Neural
 
 N = Neural()
 
-N.run_model(model ='perceptron', file_name ='ETHEREUM_PRICE.xlsx', iter= 100)
+N.run_model(model ='perceptron', file_name ='or.xlsx', iter=20, alfa=1, test_split = 0)
