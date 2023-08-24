011afc962ad8471968e14013201ca79587535aa7
alejojarah <alejojaramilloh2000@gmail.com>
2023-08-24 09:34:01 -0500

    COMMIR_PERCEPTRON_MULTI

---
+++ hubs/models/perceptronmulti.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Perceptronmulti:
+    def __init__(self):
+        pass
+    
+    def run(self,train_features, test_features, train_labels, test_labels, iter,alpha, stop_condition):
+        print('Train perceptron network....')
+        
+        hidden_neurons = train_features.shape[1] + 1
+        
+        Xi = np.zeros((train_features.shape[1], 1)) #input vector 
+        
+        Wij = np.zeros((hidden_neurons,train_features.shape[1])) #weight matrix 
+        
+        Aj = np.zeros((hidden_neurons,1)) #agregation vector 
+        
+        Hj = np.zeros((hidden_neurons + 1,1)) #neural output vector 
+        
+        Cjk = np.zeros((train_labels.shape[1],Hj.shape[0]))
+        
+        Ak = np.zeros((train_labels.shape[1],1))
+        
+        Yk = np.zeros((train_labels.shape[1],1))
+        
+        Yd = np.zeros((train_labels.shape[1],1)) #labels vector
+        
+        Ek = np.zeros((train_labels.shape[1],1))
+        
+        ecm = np.zeros((train_labels.shape[1],1)) #ECM  vector for each iteration
+        
+        ecmT = np.zeros((train_labels.shape[1],iter)) #ECM results for every iteration 
+        
+        for i in range(0,Wij.shape[0]):
+            for j in range(0, Wij.shape[1]):
+                Wij[i][j] = np.random.uniform(-1,1)
+                
+        print(f'Wij: {Wij}')
+        
+        for i in range(0,Cjk.shape[0]):
+            for j in range(0, Cjk.shape[1]):
+                Cjk[i][j] = np.random.uniform(-1,1)
+                
+        print(f'Cjk: {Cjk}')
+        
+        for it in range(0, iter):
+            for d in range(0, train_features.shape[0]):
+                ##pass the data inputs to the input vector xi
+                for i in range(0, train_features.shape[1]):
+                    Xi[i][0] = train_features[d][i]
+                    
+                ##lets calculate the agregation for each neuron 
+                for n in range(0, Aj.shape[0]):
+                    for n_input in range(0,Xi.shape[0]):
+                        Aj[n][0] = Aj[n][0] + Xi[n_input]*Wij[n][n_input]  
+                        
+                ##lets calculate the output for each neuron 
+                Hj[0][0] = 1 ##bias 
+                for n in range(0, hidden_neurons):
+                    Hj[n+1][0] = (1/(1+np.exp(-Aj[n][0])))
+                    
+                
+                for n in range(0,train_labels.shape[1]):
+                    for n_input in range(0,Hj.shape[0]):
+                        Ak[n][0] = Ak[n][0] + Hj[n_input][0]*Cjk[n][n_input]
+                        
+                for n in range(0, train_labels.shape[1]):
+                    Yk[n][0] = (1/(1+np.exp(-Aj[n][0])))
+                    
+                for i in range(0, train_labels.shape[1]):
+                    Yd[i][0] = train_labels[d][i]
+                    
+                for n in range(0, Yk.shape[0]):
+                    Ek[n][0] = Yd[n][0] - Yk[n][0]
+                    ##lets add the ECM for this data point
+                    ecm[n][0] = ecm[n][0] + (((Ek[n][0])**2)/2)
+                    
+                for n in range(0, Yk.shape[0]):
+                    for h in range(0, Hj.shape[0]):
+                        Cjk[n][h] = Cjk[n][h] + alpha*Ek[n][0]*Yk[n][0]*(1-Yk[n][0])*Hj[h][0]
+                        
+                for h in range(0, Hj.shape[0]-1):
+                    for i in range(0, Xi.shape[0]):
+                        for o in range(0, Yk.shape[0]):
+                            Wij[h][i] = Wij[h][n] + alpha*Ek[o][0]*Yk[o][0]*(1-Yk[o][0])*Cjk[o][h+1]*Hj[h+1]*(1-Hj[h+1])*Xi[i][0]
+                            
+                            
+            print(f'Iter: {it}')
+            for n in range(0, Yk.shape[0]):
+                print(f'ECM {n}: {ecm[n][0]}')
+                            
+                            
+                            
+                        
+                        
+            
+                    
+                
+                    
+
+
+
--- main.py
+++ main.py
@@ -8,4 +8,4 @@ from hubs.neural_hub import Neural
 
 N = Neural()
 
-N.run_model(model = 'perceptron', file_name = 'OR.xlsx', iter = 100, alpha = 0.1, test_split = 0, norm = False, stop_condition = 0, neurons = 1)
+N.run_model(model = 'perceptron', file_name = 'OR.xlsx', iter = 100, alpha = 0.8, test_split = 0, norm = False, stop_condition = 0, neurons = 1)
