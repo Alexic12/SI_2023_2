c81feb01788c487bd17b05c5d7dce6680e77e77a
AlexanderAntolinez <alexander201195@gmail.com>
2023-08-24 09:34:07 -0500

    COMMIT_PERCEPTRON_MULTI

---
+++ hubs/models/perceptron_multi.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class PerceptronMulti:
+    def __init__(self) -> None:
+        pass
+
+    def run (self, train_features, test_features, train_labels, test_labels,iter,alfa, stop_condition):
+        print('Training perceptron network....')
+
+        ## Lets organize the data
+
+        #arbitratry value, not absolute
+        hidden_neurons = train_features.shape[1]+ 1
+
+        Xi=np.zeros((train_features.shape[1],1)) #input vecotr 
+
+        Wij= np.zeros((hidden_neurons,train_features.shape[1])) # wieght matrix
+
+        Aj= np.zeros((hidden_neurons,1)) # Agregation vector for the hidden layer 
+
+        Hj=  np.zeros((hidden_neurons + 1,1)) #activade function for hidden layer
+
+        Cjk=  np.zeros((train_labels.shape[1],Hj.shape[0])) # wieght for outputs layer
+
+        Ak=  np.zeros((train_labels.shape[1],1))  # Agregation function for output layer
+
+        Yk= np.zeros((train_labels.shape[1],1)) # Neural Output vector 
+
+        Yd= np.zeros((train_labels.shape[1],1)) # Labels vetor
+
+        Ek= np.zeros((train_labels.shape[1],1)) # Error for output layer
+
+        ecm= np.zeros((train_labels.shape[1],1)) # ECM vector for each iteration
+
+        ecmT= np.zeros((train_labels.shape[1],iter)) # ECM results for every iteration 
+
+        ##Fill the weight Wij matrix before trainning 
+        for i in range (0,Wij.shape[0]):
+            for j in range(0,Wij.shape[1]):
+                Wij[i][j]= np.random.uniform(-1,1)
+        
+        print(f'W: {Wij}')
+
+        ##Fill the weight Cjk matrix before trainning 
+        for i in range (0,Cjk.shape[0]):
+            for j in range(0,Cjk.shape[1]):
+                Cjk[i][j]= np.random.uniform(-1,1)
+        
+        print(f'W: {Cjk}')
+
+
+        for it in range(0,iter):
+            for d in range (0,train_features.shape[0]):
+                #pass the data inputs to the input vector Xi
+                for i in range(0,train_features.shape[1]):
+                    Xi[i][0]= train_features[d][i]
+
+                
+                #lets calculate the agregation for each neuron
+                for n in range(0,Aj.shape[0]):
+                    for n_input in range(0,Xi.shape[0]):
+                        Aj[n][0] = Aj[n][0] + Xi[n_input]*Wij[n][n_input]
+
+                #lets calculate the hidden activation function for each neuron
+                Hj[0][0] = 1 #add the bias
+                for n in range(0,hidden_neurons):
+                    Hj[n+1][0] = 1/(1+np.exp(-Aj[n][0]))
+
+
+                #lets calculate the hidden agregation function for each neuron
+                for n in range(0,train_labels.shape[1]):
+                    for n_input in range(0,Hj.shape[0]):
+                        Ak[n][0]= Ak[n][0]+ Hj[n_input][0]*Cjk[n][n_input]
+
+                ## Lets calculate the activation function of output layer 
+                for n in range(0,train_labels.shape[1]):
+                    Yk[n][0] = 1/(1+np.exp(-Ak[n][0]))
+
+                ##Lets fill the Yd array with the labels for this data point 
+                for i in range(0,train_labels.shape[1]):
+                    Yd[i][0] = train_labels[d][i]
+
+                ##Lets calculate the error fot this data point
+                for n in range(0,Yk.shape[0]):
+                    Ek[n][0] = Yd[n][0]-Yk[n][0]
+                    #lets add the ECM for this data point 
+                    ecm[n][0] = ecm[n][0]+ ((Ek[n][0]**2)/2)        
+
+                ##Lets train the weights
+
+                ## Because the back propagation we first train Cjk
+
+                for n in range(0,Yk.shape[0]): #Cjk.shape[0]
+                    for h in range(0,Hj.shape[0]): #Cjk.shape[1]
+                        Cjk[n][h]= Cjk[n][h] + alfa*Ek[n][0]*Yk[n][0]*(1-Yk[n][0])*Hj[h][0]
+
+
+                ##Lets train de Wij weights
+                for h in range(0,Hj.shape[0]-1):  # or use hidden_neurons
+                    for i in range(0,Xi.shape[0]):
+                        for o in range(0,Yk.shape[0]):
+                            Wij[h][i]= Wij[h][i] + alfa*Ek[o][0]*Yk[o][0]*(1-Yk[o][0])*Cjk[o][h+1]*Hj[h+1]*(1-Hj[h+1])*Xi[i][0]
