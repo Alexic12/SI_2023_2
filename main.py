a7f59c2d8c7627988d4ef52bd6e7d34ecb1fb9d5
ParisLas <paris.mariana@hotmail.com>
2023-08-29 09:31:17 -0500

    PERCEPTRON_MULTI

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from hubs.neural_hub import Neural
 
 N = Neural()
 
-N.run_model(model = 'perceptron', file_name= 'AND_NOR.xlsx', iter=100, alfa=0.2 , test_split= 0,norm= False,stop_condition = 0,cant_neuronas =2)
+N.run_model(model = 'perceptron_multi', file_name = 'ETHEREUM_PRICE.xlsx', iter = 100, alfa=0.2, test_split = 0.1, norm = True, stop_condition = 0, neurons = 1)
