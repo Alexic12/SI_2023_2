d4dd671ecd0e4a35f0bce23e435a2b0fedabe857
Jhon-Marin <Jhon-Marin>
2023-09-04 22:00:16 -0500

    Actualizado_parcial

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from hubs.neural_hub import Neural
 
 N = Neural()
 
-N.run_model(model = 'perceptron_multi', file_name = 'ETHEREUM_PRICE.xlsx', iter = 100, alfa=0.2, test_split = 0.1, norm = True, stop_condition = 0, neurons = 1)
+N.run_model(model = 'perceptron_multi', file_name = 'HEART_DISEASE.xlsx', iter = 20, alfa=0.2, test_split = 0.1, norm = True, stop_condition = 0, neurons = 1,avoid_col=0)
 
