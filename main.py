5676385bf04a1a3fe240843136d79ce63b540cde
AlexanderAntolinez <alexander201195@gmail.com>
2023-09-05 20:44:10 -0500

    COMMIT_CORRECCION_MULTI

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ N = Neural()
 
 N.run_model(
     model = 'perceptron_multi', 
-    file_name= 'train.xlsx', 
-    iter = 20, 
+    file_name= 'BASE_DATOS.xlsx', 
+    iter = 100, 
     alfa = 0.2, 
     test_split= 0, 
     norm = True, 
     stop_condition = 0,
-    neurons = 1,
+    neurons = 4,
     avoid_col = 0
 )
