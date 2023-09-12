5e0079de08b56cb86619bc0594915369ed07948f
Florent Visconti <florent.visconti@gmail.com>
2023-09-12 09:38:58 -0500

    ffm_tf final

--- hubs/models/ffm_tf.py
+++ hubs/models/ffm_tf.py
@@ -22,32 +22,46 @@ class ffm_tf:
     def run(self, train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition):
         model = self.build_model(train_features.shape[1] + 1, train_labels.shape[1], alfa)
 
-        ##Lets make a stop function
+        ##lets make an stop function
         early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=stop_condition)
 
-        ##Lets train the model
-        history = model.fit(train_features, train_labels, epochs=iter, validation_split = 0, verbose = 1, callbacks=[early_stop])
+        ##lets train the model
+        history = model.fit(train_features, train_labels, epochs=iter, verbose = 1, callbacks = [early_stop], validation_split=0)
 
-        print(history)
+        training_data = pd.DataFrame(history.history)
+        print(training_data)
+
+        plt.figure()
+        plt.plot(training_data['mse'], 'r', label = 'mse')
+        plt.show()
+
+
+        ##Lets validate the trained model
+        pred_out = model.predict(test_features)
+
+        plt.figure()
+        plt.plot(pred_out, 'r', label = 'Prediction Output')
+        plt.plot(test_labels, 'b', label = 'Real Output')
+        plt.legend()
+        plt.show()
 
 
 
-
-    def build_model(self, layers, hidden_neurons, output, alfa):
+    def build_model(self, hidden_neurons, output, alfa):
         model = keras.Sequential([
-            Dense(hidden_neurons, activation= tf.nn.sigmoid, input_shape=[hidden_neurons-1]),
-            Dense(hidden_neurons, activation= tf.nn.sigmoid),
-            Dense(hidden_neurons, activation= tf.nn.sigmoid),
-            Dense(hidden_neurons, activation= tf.nn.sigmoid),
-            Dense(hidden_neurons, activation= tf.nn.sigmoid),
-            Dense(hidden_neurons, activation= tf.nn.sigmoid),
-            Dense(hidden_neurons, activation= tf.nn.sigmoid),
-            Dense(hidden_neurons, activation= tf.nn.sigmoid),
-            Dense(hidden_neurons, activation= tf.nn.sigmoid),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid, input_shape=[hidden_neurons-1]),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid),
+            Dense(hidden_neurons, activation=tf.nn.sigmoid),
             Dense(output)
-            
+
         ])
 
-        model.compile(optimizer= keras.optimizers.Adam(learning_rate = alfa), loss='mse', metrics=['mse'])
+        model.compile(optimizer=keras.optimizers.Adam(learning_rate=alfa), loss='mse', metrics=['mse'])
 
         return model
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from hubs.neural_hub import Neural
 
 N = Neural()
 
-N.run_model(model='ffm_tf', file_name= 'ETHEREUM_PRICE.xlsx', iter = 500, alfa = 0.02, test_split = 0.1, norm = False, stop_condition = 50, neurons = 1, avoid_cols = 0)
+N.run_model(model = 'ffm_tf', file_name = 'HEART_DISEASE_DB.xlsx', iter = 500, alfa=0.002, test_split = 0.1, norm = True, stop_condition = 50, neurons = 1, avoid_cols = 0)
 
 
 
