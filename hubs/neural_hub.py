56b7d7ada5debb83dbc2946c412589b0bc55941f
Alexic12 <alejandropuerta@outlook.com>
2023-09-12 09:11:41 -0500

    tensorflow working

--- hubs/neural_hub.py
+++ hubs/neural_hub.py
@@ -2,15 +2,16 @@
 from hubs.data_hub import Data
 from hubs.models.perceptron import Perceptron
 from hubs.models.perceptron_multi import PerceptronMulti
+from hubs.models.ffm_tf import ffm_tf
 
 
 class Neural:
     def __init__(self):
         pass
 
-    def run_model(self, model, file_name, iter, alfa, test_split, norm, stop_condition, neurons):
+    def run_model(self, model, file_name, iter, alfa, test_split, norm, stop_condition, neurons, avoid_col):
         data = Data()
-        train_features, test_features, train_labels, test_labels = data.data_process(file_name, test_split,norm, neurons)
+        train_features, test_features, train_labels, test_labels = data.data_process(file_name, test_split,norm, neurons, avoid_col)
         if model == 'perceptron':
             print('Running Perceptron Model')
             ##Code for the perceptron model
@@ -18,9 +19,11 @@ class Neural:
             P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
 
 
-        elif model == 'ffm':
+        elif model == 'ffm_tf':
             print('Running FFM Model')
             ##Code for the perceptron model
+            P = ffm_tf()
+            P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
 
         elif model == 'perceptron_multi':
             print('Running perceptron Multi Model')
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from hubs.neural_hub import Neural
 
 N = Neural()
 
-N.run_model(model = 'perceptron_multi', file_name = 'ETHEREUM_PRICE.xlsx', iter = 100, alfa=0.2, test_split = 0.1, norm = True, stop_condition = 0, neurons = 1)
+N.run_model(model = 'ffm_tf', file_name = 'ETHEREUM_PRICE.xlsx', iter = 500, alfa=0.02, test_split = 0.1, norm = True, stop_condition = 50, neurons = 1, avoid_col = 0)
 
