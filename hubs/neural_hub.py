ce28eb94508d230192e1010fbdd205a0605c90c9
MauPos04 <mauriciop1palma@gmail.com>
2023-09-16 08:03:55 -0500

    XGBOOST working well

--- hubs/neural_hub.py
+++ hubs/neural_hub.py
@@ -1,43 +1,34 @@
+
 from hubs.data_hub import Data
 from hubs.models.perceptron import Perceptron
-# from hubs.models.perceptron_mult import Perceptron_mult
 from hubs.models.perceptron_multi import PerceptronMulti
+from hubs.models.ffm_tf import ffm_tf
+from hubs.models.xgboost import xgb
+
 
 class Neural:
-    def __init__(self) :
+    def __init__(self):
         pass
 
-
-    # def run_model (self, model, file_name,iter , alfa, test_split, norm, stop_condition,nfl,neurons):
     def run_model(self, model, file_name, iter, alfa, test_split, norm, stop_condition, neurons, avoid_col):
-
         data = Data()
-
-        train_features, test_features, train_labels, test_labels  = data.data_process(file_name,test_split, norm, neurons, avoid_col)
-
-        if model == 'perceptron' :
-            print ('Running Perceptron Model')
-
-            ##Code for the perceptron model
-
+        train_features, test_features, train_labels, test_labels = data.data_process(file_name, test_split,norm, neurons, avoid_col)
+        if model == 'perceptron':
+            print('Running Perceptron Model')
             P = Perceptron()
-            P.run(train_features, test_features, train_labels, test_labels,iter, alfa, stop_condition)
+            P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
 
-        elif model == 'ffm' : 
-            print ('Running FFM Model')
+        elif model == 'ffm_tf':
+            print('Running FFM Model')
+            P = ffm_tf()
+            P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
 
-            ##Code for the ffm model
-
-
-
-        # elif model == 'perceptron_mult': ##El que hicimos propio
-        #     print("Running Perceptron multicapa")
-        #     #Code for the perceptron multicap model
-        #     Pm = Perceptron_mult()
-        #     Pm.run(train_features, test_features, train_labels, test_labels,iter, alfa, stop_condition, nfl)
-
-        elif model =='perceptron_multi': ##El del profe
-            print('Running perceptron multi model ')
-
+        elif model == 'perceptron_multi':
+            print('Running perceptron Multi Model')
             P = PerceptronMulti()
-            P.run(train_features,test_features,train_labels,test_labels, iter, alfa, stop_condition)
+            P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
+
+        elif model == 'xgb':
+            print('Running XGBoost model')
+            P = xgb(depth = 10)
+            P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from hubs.neural_hub import Neural
 N = Neural()
 
 # N.run_model(model = 'perceptron_multi', file_name = 'ETHEREUM_PRICE.xlsx', iter = 100, alfa= 0.2, test_split = 0, norm=True, stop_condition=0.5, nfl = 2, neurons = 1)
-N.run_model(model = 'perceptron_multi', file_name = 'train_heart.xlsx', iter = 40, alfa=0.2, test_split = 0.1, norm = True, stop_condition = 0, neurons = 1, avoid_col = 1)
+N.run_model(model = 'xgb', file_name = 'HEART_DISEASE_DB.xlsx', iter = 500, alfa=0.1, test_split = 0.1, norm = True, stop_condition = 50, neurons = 1, avoid_col = 0)
 
 ##avoid column cuantas columnas quiero evitar al principio
 
