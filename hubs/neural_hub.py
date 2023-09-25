a18c3944d47bdb5399544859b31deec8e293bc4e
Jhon-Marin <Jhon-Marin>
2023-09-25 11:47:26 -0500

    WORKING XGBOOST PRODUCTION MODEL

--- hubs/neural_hub.py
+++ hubs/neural_hub.py
@@ -2,37 +2,37 @@
 from hubs.data_hub import Data
 from hubs.models.perceptron import Perceptron
 from hubs.models.perceptron_multi import PerceptronMulti
-from hubs.models.ffm_tf import ffm_tf 
+from hubs.models.ffm_tf import ffm_tf
+from hubs.models.xgboost import xgb
+
 
 class Neural:
     def __init__(self):
         pass
 
-    def run_model(self, model, file_name, iter, alfa, test_split, norm, stop_condition, neurons):
+    def run_model(self, model, file_name, iter, alfa, test_split, norm, stop_condition, neurons, avoid_col, chk_name, train):
         data = Data()
-        train_features, test_features, train_labels, test_labels = data.data_process(file_name, test_split,norm, neurons)
+        train_features, test_features, train_labels, test_labels, original_features, original_labels = data.data_process(file_name, test_split,norm, neurons, avoid_col)
         if model == 'perceptron':
             print('Running Perceptron Model')
-            ##Code for the perceptron model
             P = Perceptron()
             P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
 
-
-        elif model == 'ffm':
+        elif model == 'ffm_tf':
             print('Running FFM Model')
-            ##Code for the perceptron model
+            P = ffm_tf()
+            P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
 
         elif model == 'perceptron_multi':
             print('Running perceptron Multi Model')
-
             P = PerceptronMulti()
             P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
 
-        elif model == 'ffm_tf':
-            print('Running ffm Model')
+        elif model == 'xgb':
+            print('Running XGBoost model')
+            P = xgb(depth = 10)
+            P.run(train_features, test_features, train_labels, test_labels, original_features, original_labels, iter, alfa, stop_condition, chk_name, train, neurons)
 
-            P= ffm_tf()
-            P.run(train_features, test_features, train_labels, test_labels, iter, alfa, stop_condition)
 
 
 
