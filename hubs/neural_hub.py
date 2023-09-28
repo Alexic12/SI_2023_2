55ed392e26d42a04d8eca78609a63794f79ecbfc
Alexic12 <alejandropuerta@outlook.com>
2023-09-28 09:28:48 -0500

    WORKING TIME SERIES PROCESS FROM DATAHUB

--- hubs/neural_hub.py
+++ hubs/neural_hub.py
@@ -11,12 +11,18 @@ class Neural:
     def __init__(self):
         pass
 
-    def run_model(self, model, file_name, iter, alfa, test_split, norm, stop_condition, neurons, avoid_col, chk_name, train):
+    def run_model(self, model, file_name, iter, alfa, test_split, norm, stop_condition, neurons, avoid_col, chk_name, train, data_type):
         data = Data()
         if model == 'conv_tf':
             train_images, test_images, train_labels, test_labels = data.download_database('MNIST')
         else:
-            train_features, test_features, train_labels, test_labels, original_features, original_labels = data.data_process(file_name, test_split,norm, neurons, avoid_col)
+            if data_type == 'time_series':
+                window_size = 3
+                horizon_size = 1
+                train_features, test_features, train_labels, test_labels, original_features, original_labels = data.timeseries_process(window_size, horizon_size, file_name, test_split, norm)
+
+            elif data_type == 'data':
+                train_features, test_features, train_labels, test_labels, original_features, original_labels = data.data_process(file_name, test_split,norm, neurons, avoid_col)
         
         
         if model == 'perceptron':
