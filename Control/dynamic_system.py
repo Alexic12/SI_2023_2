0d49b5511a711954c29433b4761ee99acaccc0cf
alejojarah <alejojaramilloh2000@gmail.com>
2023-10-05 10:29:36 -0500

    commit_dynamic_system_2

--- Control/dynamic_system.py
+++ Control/dynamic_system.py
@@ -1,148 +1,129 @@
 import numpy as np
-import pandas as pd 
+import pandas as pd
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import random
 
-
 class MassDamper:
-    
+
     def __init__(self):
+
+        self.m = 10  # Masa del objeto 
         
-        self.m = 2 ##masa del objeto
+        self.k = 0.2  # Constante del resorte
         
-        self.k = 0.5 ##constante del resorte
+        self.c = 2  # Coeficiente de fricción
         
-        self.c = 2 ##coeficiente de friccion
+        self.n = 2000  # Número de tiempo por simulación
         
-        self.n = 2000 ##numero de tiempo por simulacion
+        self.s_t = 1000  # Tiempo de muestreo
         
-        self.s_t = 1000 ##tiempo de muestreo 
+        self.x0 = 0  # Posición inicial
         
-        self.x0 = 0 ##posicion inicial
+        self.v0 = 0  # Velocidad inicial
         
-        self.v0 = 0 ##velocidad inicial
+        self.step_interval = 100  # Tiempo de muestreo en ms
         
-        self.step_interval = 100 ##tiempo de muestreo en ms
+        self.last_update_time = -self.step_interval  # Última actualización de tiempo para la simulación
         
-        self.last_update_time = -self.step_interval ##ultima actualizacion de tiempo para la simulacion
-        
-        self.U = 0 ##system input
-        
-        
+        self.U = 0  # System input
+
     def update_force(self, t):
-        
-        if t-self.last_update_time >= self.step_interval:
+
+        if t - self.last_update_time >= self.step_interval:
             
-            self.U = random.uniform(-1,1)
+            self.U = random.uniform(-1, 1)
             
             self.last_update_time = t
-            
-    def system_equiations(self, t, y):
+
+    def system_equations(self, t, y):
         
-        x, v = y ##posicion y velocidad actual 
+        x, v = y  # Posición y velocidad actual
         
         dxdt = v
         
-        dvdt = -((self.k/self.m)*x - (self.c/self.m)*v + (self.U/self.m))
+        dvdt = -(self.k / self.m) * x - (self.c / self.m) * v + (self.U / self.m)
         
         return [dxdt, dvdt]
-    
+
     def run_simulation(self):
-        
-        ##set initial condition and simulation time
-        
-        y0 =[self.x0, self.v0]
+
+        # Establecer condiciones iniciales y tiempo de simulación
+        y0 = [self.x0, self.v0]
         
         time_span = [0, self.n]
-        
-        ##resolver las ecuaciones del sistema
-        
-        sol = solve_ivp(self.system_equiations, time_span, y0, dense_output = True)
-        
-        ##create time points for simulation 
-        
-        t = np.linspace(0, self.n, num = self.s_t)
-        
-        ##obtener posicion y velocidad para todos los puntos en el tiempo 
-        
+
+        # Resolver las ecuaciones del sistema
+        sol = solve_ivp(self.system_equations, time_span, y0, dense_output=True)
+
+        # Crear puntos de tiempo para la simulación
+        t = np.linspace(0, self.n, num=self.s_t)
+
+        # Obtener posición y velocidad para todos los puntos en el tiempo
         x, v = sol.sol(t)
-        
-        plt.figure(figsize = (12,6))
+
+        plt.figure(figsize=(12, 6))
         
         plt.xlabel('Tiempo [ms]')
         
-        plt.ylabel('Value')
+        plt.ylabel('Valor')
         
-        plt.title('mass damper simulation system')
+        plt.title('Simulación de sistema masa-amortiguador')
         
         plt.grid()
+
+        # Inicializar líneas para posición, velocidad y U
+        position_line, = plt.plot([], [], label='Posición (x)')
         
-        ##initial lines for posicion, velocity and U
+        velocity_line, = plt.plot([], [], label='Velocidad (v)')
         
-        position_line, = plt.plot([], [], label = 'position (x)')
-        
-        velocity_line, = plt.plot([], [], label = 'Velocity (v)')
-        
-        input_line, = plt.plot([], [], label = 'system input (u)')
-        
+        input_line, = plt.plot([], [], label='Entrada del sistema (u)')
+
         plt.legend()
+
+        # Inicializar vectores de posición, velocidad y U
+        x_data = np.zeros(len(t))
         
-        ##initial position and velocity vector
+        v_data = np.zeros(len(t))
         
-        x = np.zeros(len(t))
-        
-        v = np.zeros(len(t))
-        
-        U = np.zeros(len(t))
-        
-        ##lets simulate the system for the simulation time
-        
+        U_data = np.zeros(len(t))
+
+        # Simular el sistema durante el tiempo de simulación
         for i in range(1, len(t)):
+            # Actualizar la fuerza si queremos identificar el sistema
+            self.update_force(t[i - 1])
+
+            # Almacenar la entrada del sistema
+            U_data[i - 1] = self.U
+
+            # Establecer condiciones iniciales y tiempo de simulación
+            y0 = [x_data[i - 1], v_data[i - 1]]
             
-            ##if we want to identify the system lets update the force
+            time_span = [t[i - 1], t[i]]
+
+            # Resolver las ecuaciones del sistema para esta iteración
+            sol = solve_ivp(self.system_equations, time_span, y0, dense_output=True)
+
+            # Obtener la posición y velocidad para el próximo tiempo de muestreo
+            x_data[i], v_data[i] = sol.sol(t[i])
+
+            # Actualizar las líneas del gráfico para mostrar el estado del sistema
+            position_line.set_data(t[:i + 1], x_data[:i + 1])
             
-            self.update_force(t[i-1])
+            velocity_line.set_data(t[:i + 1], v_data[:i + 1])
             
-            ##store the system input
-            
-            U[i-1] = self.U
-            
-            ##set initial conditions and timespan
-            
-            y0 = [x[i-1], v[i-1]] 
-            
-            time_span = [t[i-1], t[i]]  
-            
-            ##solve the system equations for this iteration
-            
-            sol = solve_ivp(self.system_equiations, time_span, y0, dense_output = True)
-            
-            ##get the pisition and velocity for the nex sample time
-            
-            x[i], v[i] = sol.sol(t[i])
-            
-            ##lets update the graph lines for showing system status
-            
-            position_line.set_data(t[i+1], x[i+1])
-            
-            velocity_line.set_data(t[i+1], v[i+1])
-            
-            input_line.set_data(t[i+1], U[i+1])
-            
-            ##lets show the data up until the actual sample time
-            
+            input_line.set_data(t[:i + 1], U_data[:i + 1])
+
+            # Mostrar datos hasta el tiempo de muestreo actual
             plt.xlim(0, t[i])
             
-            plt.ylim(min(min(x), min(v), min(U)) - 0.5, max(max(x), max(v), max(U)) + 0.5)
-            
-            ##lets pause the graph
-            
+            plt.ylim(min(x_data[:i + 1]) - 0.5, max(x_data[:i + 1]) + 0.5)
+
+            # Pausar el gráfico brevemente
             plt.pause(self.n/self.s_t)
-            
-        plt.show()  
-            
-            
+
+    plt.show()
+
 s = MassDamper()
 
-s.run_simulation()               
+s.run_simulation()
