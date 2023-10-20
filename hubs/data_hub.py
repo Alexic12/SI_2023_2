0ca87238a9a9f628a87d1be7a182024e058d1c42
VinyMilk5869 <santiagomontoyahernandez1@gmail.com>
2023-10-19 19:19:03 -0500

    ACTUALIZACION_DYNAMIC_SYSTEM

--- hubs/data_hub.py
+++ hubs/data_hub.py
@@ -1,9 +1,6 @@
 import os
 import sys
-import tensorflow as tf 
-from tensorflow import keras 
-from keras.layers import Dense
-##MPP
+
 import pandas as pd
 import numpy as np
 
@@ -13,6 +10,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split as tts
 
+import tensorflow
+from tensorflow import keras
 
 class Data:
     """
@@ -107,22 +106,20 @@ class Data:
         data_denorm = self.scaler.inverse_transform(data)
 
         return data_denorm
-    
 
 
-    def download_database(self,database):
-        if database == "MNIST":
-            (train_images, train_labels), (test_images, test_labels)=keras.datasets.mnist.load_data()
+    def download_database(self, database):
+        if database == 'MNIST':
+            (train_images, train_labels),(test_images, test_labels) = keras.datasets.mnist.load_data()
 
-        elif database =="CIFAR10":
+        elif database == 'CIFAR10':
+            pass
+        elif database == 'CIFAR100':
             pass
 
-        elif database == "CIFAR100":
-            pass
-
-        return train_images, test_images, train_labels, test_labels
+        return train_images, test_images, train_labels, test_labels 
     
-    def time_series_process(self,window_size, horizon_size,file , test_split, norm, direct):
+    def timeseries_process(self, window_size, horizon_size, file, test_split, norm):
         ##Lets define the absolute path for this folder
         data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','data'))
 
@@ -135,37 +132,30 @@ class Data:
         ##lets turn it into an array
         array_raw = np.array(data_raw)
 
-        ##lets get the number of sample times for the whole data (# of sample times)
-        data_lenght = array_raw.shape[1]
+        ## Lets get the number of sample times for the whole data (# of sample times)
+        data_length = array_raw.shape[1]
 
-        print(f'Sample times for time series: {data_lenght}')
+        print(f'Sample Times for time series: {data_length}')
 
-        ##lets create the data base array for storing the data the proper way
-        ##Rows = (#Sample times - window-horizon)+1
-        ##Columns = (window+horizon)
-        time_series_arr = np.zeros((data_lenght-window_size-horizon_size+ 1, window_size*2+horizon_size+1))
+        ##Lets create the data base array for storing the data the proper way
+        ##Rows = (#Sample Times - Window - horizon)
+        ##Columns = (window + horizon)
+        time_series_arr = np.zeros((data_length - window_size - horizon_size + 1, window_size*2 + horizon_size + 1))
 
-        print(f'Time_Series_arra rows: {time_series_arr.shape[0]}')
-        print(f'Data array raw Shape: {array_raw.shape}')
-        print(f'Time_series_arr shape: {time_series_arr.shape}')
+        print(f'Time_Series_Arr rows: {time_series_arr.shape[0]}')
+        print(f'Data Array Raw Shape: {array_raw.shape}')
+        print(f'Time_Series_Arr Shape: {time_series_arr.shape}')
 
-
-        ##we have to look through all the raw data , and take the correct data points and store them as windows and horizon
-        ## Prueba de control directo
-        if direct == True:
-            for i in range (data_lenght-window_size-horizon_size):
-                vector = np.concatenate((array_raw[0, i:i+window_size+horizon_size], array_raw[1, i:i+window_size+horizon_size]))
-                time_series_arr[i] = vector
-
-        elif direct == False :
-            for i in range (data_lenght-window_size-horizon_size):
-                vector = np.concatenate((array_raw[1, i:i+window_size+horizon_size], array_raw[0, i:i+window_size+horizon_size]))
-                time_series_arr[i] = vector
+        ##we have to look trough all the raw data, and take the correct data points and store them as window and horizon
+        for i in range(data_length - window_size - horizon_size):
+            vector = np.concatenate((array_raw[0, i:i+window_size+horizon_size], array_raw[1, i:i+window_size+horizon_size]))
+            time_series_arr[i] = vector
 
         ##lets print this time_series_arr
-        print(f'TimeSeries')
+        print('Time Series')
         print(time_series_arr)
 
+        
         ##lets store the original features
         columns = time_series_arr.shape[1]
         original_features = data_raw[data_raw.columns[0:-horizon_size]]
@@ -174,30 +164,24 @@ class Data:
         print(f'Original_labels {original_labels}')
 
         ##now that we have the time series as a normal database for neural networks we have to split it on features and labels
-        data_features = time_series_arr[:,0:-horizon_size]
-        data_labels = time_series_arr[:,-horizon_size:]
+        data_features = time_series_arr[:, 0:-horizon_size]
+        data_labels = time_series_arr[:, -horizon_size:]
 
-        ##lets normalize the data 
-
-
-        ##lets split the data into training and testing
+        ##lets normalize the data
+        
         if norm == True:
             ##Lets normalize the data
             if horizon_size == 1:
-                data_labels = data_labels.reshape(-1,1) ## toca volverlo 2 dimensional para que no saque error
-                
+                data_labels = data_labels.reshape(-1,1)
             sc = StandardScaler()
             data_features_norm = self.scaler.fit_transform(data_features)
             data_labels_norm = self.scaler.fit_transform(data_labels)    
         else:
             data_features_norm = data_features
-            data_labels_norm = data_labels 
+            data_labels_norm = data_labels
+
         ##lets split the data into training and testing
         ##input (train, test) output (train, test)
-
-
-        ## Prueba de control directo 
-
 
         if test_split != 0:
             train_features, test_features, train_labels, test_labels = tts(data_features_norm, data_labels_norm, test_size=test_split)
@@ -206,13 +190,12 @@ class Data:
             test_labels = 0
             train_features = data_features_norm
             train_labels = data_labels_norm
-            # print(f'Features: {train_features}')
-            # print(f'labels: {train_labels}')     
+            #print(f'Features: {train_features}')
+            #print(f'labels: {train_labels}')
 
-        return   train_features, test_features, train_labels, test_labels , original_features, original_labels
+        return train_features, test_features, train_labels, test_labels, original_features, original_labels
 
 
 ##Lets run this method of time_series just for testing
-# T = Data()
-# T.time_series_process(3,1,'DATA_SENO_DIRECTO.xlsx')
-            
+##T = Data()
+##T.timeseries_process(3,1,'DATA_SENO_DIRECTO.xlsx')
--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from hubs.neural_hub import Neural
 
-##MPP
+
 N = Neural()
 
 model = 'xgb'
-file_name = 'DATA_SENO_DIRECTO.xlsx'
-iter = 20
+file_name = 'datos.xlsx'
+iter = 2
 alfa=0.7
 test_split = 0.1
 norm = True
 stop_condition = 50
 output = 1
 avoid_col = 0
-chk_name = 'SENO_1'
+chk_name = 'PID_IDENT_1'
 train = True
-data_type='time_series'
-direct = False
+data_type = 'time_series'
 
-N.run_model(model = model, file_name = file_name, iter = iter, alfa=alfa, test_split = test_split, norm = norm, stop_condition = stop_condition, neurons = output, avoid_col = avoid_col, chk_name = chk_name, train = train, data_type=data_type, direct = direct )
-## Avoid columns para quitar la ultima columna
+
+N.run_model(model = model, file_name = file_name, iter = iter, alfa=alfa, test_split = test_split, norm = norm, stop_condition = stop_condition, neurons = output, avoid_col = avoid_col, chk_name = chk_name, train = train, data_type = data_type)
