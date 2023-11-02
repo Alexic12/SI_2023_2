40d011903d2defdafd3f743107166c3e602dfed9
ParisLas <paris.mariana@hotmail.com>
2023-11-02 09:37:16 -0500

    COMMIT FINAL

--- control/dynamic_system.py
+++ control/dynamic_system.py
@@ -35,7 +35,8 @@ class MassDamper:
         ##las value for filling sp arr
         self.last_value = 0
         ##lets load the neural model
-        self.model = xgb.load_model('PID_IDENT_1', 7, 0.02)
+        xg = xgb(10)
+        self.model = xg.load_model(name = 'INDENT_DIR_PID',inputs =  7, alfa = 0.02)
 
     def update_force(self, t, type, force):
 
@@ -88,6 +89,8 @@ class MassDamper:
         ##fill the 
         sp_arr = self.fill_sp(len(sp_arr), -1, 1, 100)
 
+        #Create error vector
+        err_arr = np.zeros(len(t))
 
         ##get the position and velocity data for the time points
         x, v = sol.sol(t)
@@ -104,6 +107,7 @@ class MassDamper:
         velocity_line, = plt.plot([],[],label='Velocity (V)')
         input_line, = plt.plot([],[],label='System Input (U)')
         sp_line, = plt.plot([],[],label='System SetPoint (SP)')
+        err_line, = plt.plot([],[],label='System Error (ERR)')
 
         ##lets show graph legend
         plt.legend()
@@ -120,24 +124,59 @@ class MassDamper:
         for i in range(1, len(t)):
             ##if we want to identify the system lets update the force
 
-            ##self.update_force(t[i-1])
-            ##self.pid_control(x[i-1], sp_arr[i-1])
+            #self.update_force(t[i-1], 'random', 0)
+            #self.pid_control(x[i-1], sp_arr[i-1])
 
-            control_vector[0] = 2 ##setpoint
+            ##lets store the error for that specific time sample
+            err_arr[i-1] = sp_arr[i-1] - x[i-1]
 
-            ##for storing the position
+            control_vector[0] = control_vector[1]
+            control_vector[1] = sp_arr[i-1]
+            control_vector[2] = control_vector[3]
+            control_vector[3] = err_arr[i-1]
+            control_vector[4] = control_vector[5]
+            control_vector[5] = x[i-1]
+            control_vector[6] = U[i] ##setpoint
+
+            '''
+            #for storing the position setpoint
+            control_vector[0] = control_vector[1]
             control_vector[1] = control_vector[2]
             control_vector[2] = control_vector[3]
-            control_vector[3] = x[i-1]
-
-            ##for storing the control input
+            control_vector[3] = control_vector[4]
             control_vector[4] = control_vector[5]
             control_vector[5] = control_vector[6]
-            control_vector[6] = U[i-1]
-            
-            ##lets perform the control action
-            self.U = self.inverse_neuronal_control(control_vector)
+            control_vector[6] = sp_arr[i-1] ##setpoint
 
+            ##for storing the error 
+            control_vector[7] = control_vector[8]
+            control_vector[8] = control_vector[9]
+            control_vector[9] = control_vector[10]
+            control_vector[10] = control_vector[11]
+            control_vector[11] = control_vector[12]
+            control_vector[12] = control_vector[13]
+            control_vector[13] = err_arr[i-1] #error
+
+            #for storing the position
+            control_vector[14] = control_vector[15]
+            control_vector[15] = control_vector[16]
+            control_vector[16] = control_vector[17]
+            control_vector[17] = control_vector[18]
+            control_vector[18] = control_vector[19]
+            control_vector[19] = control_vector[20]
+            control_vector[20] = x[i-1] ##position
+            #for storing the control input
+            control_vector[21] = control_vector[22]
+            control_vector[22] = control_vector[23]
+            control_vector[23] = control_vector[24]
+            control_vector[24] = control_vector[25]
+            control_vector[25] = control_vector[26]
+            control_vector[26] = U[i] ##setpoint
+            '''
+
+            ##lets perform the control action
+            self.U = self.inverse_neuronal_control(control_vector, U[i-1])
+            
             ##fill index
             index[i-1] = i
 
@@ -160,26 +199,27 @@ class MassDamper:
             velocity_line.set_data(t[:i+1], v[:i+1])
             input_line.set_data(t[:i+1], U[:i+1])
             sp_line.set_data(t[:i+1], sp_arr[:i+1])
+            err_line.set_data(t[:i+1], err_arr[:i+1])
 
             ##lets show te data up until the actual sample time
             plt.xlim(0, t[i])
             plt.ylim(min(min(x), min(v), min(U)) - 0.5, max(max(x), max(v), max(U)) + 0.5)
 
             ##lets pause the graph 
-            #plt.pause(self.N/self.s_t)
+            ##plt.pause(self.N/self.s_t/100000)
 
-        '''
-        data = np.vstack((x, U, sp_arr))
+        
+        data = np.vstack((x, U, sp_arr, err_arr))
         print(data)
         # Create a DataFrame from the data
         df = pd.DataFrame(data)
 
         # Save the DataFrame to an Excel file
-        excel_filename = 'your_data.xlsx'
+        excel_filename = 'TOMA_DATOS_PID_DIR.xlsx'
         df.to_excel(excel_filename, index=False)
 
         print(f'Data saved to {excel_filename}')
-        '''
+        
         plt.show()
 
 
@@ -205,6 +245,19 @@ class MassDamper:
 
         ##we store the previous value of the error
         self.error_previous = error
+
+    def inverse_neuronal_control(self, control_vector, label):
+
+        label = label.reshape((1,1))
+        control_vector = control_vector.reshape((1,7))
+
+        ##lets create an evaluation set
+        #history = self.model.fit(control_vector, label)
+        U = self.model.predict(control_vector)
+
+        
+        
+        return U[0]
 
 
 
--- hubs/data_hub.py
+++ hubs/data_hub.py
@@ -212,6 +212,100 @@ class Data:
 
         return train_features, test_features, train_labels, test_labels, original_features, original_labels
 
+
+    
+    def timeseries_process_adapt(self, window_size, horizon_size, file, test_split, norm):
+        ##Lets define the absolute path for this folder
+        data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','data'))
+
+        ##Find the complete excel file route
+        excel_path = os.path.join(data_dir, file)
+
+        ##lets load the raw excel file
+        data_raw = pd.read_excel(excel_path, sheet_name = 0)
+
+        ##lets turn it into an array
+        array_raw = np.array(data_raw)
+
+        ## Lets get the number of sample times for the whole data (# of sample times)
+        data_length = array_raw.shape[1]
+
+        print(f'Sample Times for time series: {data_length}')
+
+        ##Lets create the data base array for storing the data the proper way
+        ##Rows = (#Sample Times - Window - horizon)
+        ##Columns = (window + horizon)
+        time_series_arr = np.zeros((data_length - window_size - horizon_size + 1, window_size*4 + horizon_size + 3))
+
+        print(f'Time_Series_Arr rows: {time_series_arr.shape[0]}')
+        print(f'Data Array Raw Shape: {array_raw.shape}')
+        print(f'Time_Series_Arr Shape: {time_series_arr.shape}')
+
+        ##we have to look trough all the raw data, and take the correct data points and store them as window and horizon
+        for i in range(data_length - window_size - horizon_size):
+            vector = np.concatenate((array_raw[0, i:i+window_size+horizon_size], array_raw[3, i:i+window_size+horizon_size], array_raw[2, i:i+window_size+horizon_size], array_raw[1, i:i+window_size+horizon_size]))
+            time_series_arr[i] = vector
+
+        ##lets print this time_series_arr
+        print('Time Series')
+        print(time_series_arr)
+
+        
+        ##lets store the original features
+        columns = time_series_arr.shape[1]
+        original_features = data_raw[data_raw.columns[0:-horizon_size]]
+        original_labels = data_raw[data_raw.columns[-horizon_size:]]
+        print(f'Original_features {original_features}')
+        print(f'Original_labels {original_labels}')
+
+        ##now that we have the time series as a normal database for neural networks we have to split it on features and labels
+        data_features = time_series_arr[:, 0:-horizon_size]
+        data_labels = time_series_arr[:, -horizon_size:]
+
+        ##lets take the max value of the data
+        self.max_value = max(data_labels)
+
+        ##lets take the number of features
+        self.array_size = data_features.shape[1]
+
+        ##lets normalize the data
+        
+        if norm == True:
+            ##Lets normalize the data
+            if horizon_size == 1:
+                data_labels = data_labels.reshape(-1,1)
+            sc = StandardScaler()
+            data_features_norm = self.scaler.fit_transform(data_features)
+            data_labels_norm = self.scaler.fit_transform(data_labels)    
+        else:
+            data_features_norm = data_features
+            data_labels_norm = data_labels
+
+        ##lets split the data into training and testing
+        ##input (train, test) output (train, test)
+
+        if test_split != 0:
+            train_features, test_features, train_labels, test_labels = tts(data_features_norm, data_labels_norm, test_size=test_split)
+        else:
+            test_features = 0
+            test_labels = 0
+            train_features = data_features_norm
+            train_labels = data_labels_norm
+            #print(f'Features: {train_features}')
+            #print(f'labels: {train_labels}')
+
+        data = np.hstack((train_features, train_labels))
+        #print(data)
+        # Create a DataFrame from the data
+        df = pd.DataFrame(data)
+
+        # Save the DataFrame to an Excel file
+        excel_filename = 'DATA_PID_ORGANIZADA_ADAPT.xlsx'
+        df.to_excel(excel_filename, index=False)
+
+        return train_features, test_features, train_labels, test_labels, original_features, original_labels
+
+
     def get_max_value(self):
         return self.max_value
     
@@ -221,4 +315,3 @@ class Data:
 ##Lets run this method of time_series just for testing
 ##T = Data()
 ##T.timeseries_process(3,1,'DATA_SENO_DIRECTO.xlsx')
-            
