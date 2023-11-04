cec4523312cb4f2e716abc8f37ade72ca925ce6b
MauPos04 <mauriciop1palma@gmail.com>
2023-11-04 12:52:49 -0500

    BEFORE_EXAM1

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from hubs.neural_hub import Neural
 
 
 N = Neural()
-#MPP
+
 model = 'xgb'
-file_name = 'TOMA_DATOS_PID_2.xlsx'
+file_name = 'TOMA_DATOS_DIR.xlsx'
 iter = 2
 alfa=0.2
 test_split = 0.1
@@ -12,11 +12,9 @@ norm = True
 stop_condition = 50
 output = 1
 avoid_col = 0
-chk_name = 'PID_IDENT_1'
+chk_name = 'INDENT_DIR_PID'
 train = True
 data_type = 'time_series'
-direct = True
 
 
-N.run_model(model = model, file_name = file_name, iter = iter, alfa=alfa, test_split = test_split, norm = norm, stop_condition = stop_condition, neurons = output, avoid_col = avoid_col, chk_name = chk_name, train = train, data_type = data_type, direct =direct)
-## Avoid columns para evadir la ultima o columnas
+N.run_model(model = model, file_name = file_name, iter = iter, alfa=alfa, test_split = test_split, norm = norm, stop_condition = stop_condition, neurons = output, avoid_col = avoid_col, chk_name = chk_name, train = train, data_type = data_type)
