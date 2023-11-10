ae8855928eeb7685bf119c6dfdee1ad8dd7b6dec
ldanielt <luis.payares@eia.edu.co>
2023-11-10 10:49:11 -0500

    COMMIT FINAL

--- hubs/models/xgboost.py
+++ hubs/models/xgboost.py
@@ -32,6 +32,9 @@ class xgb:
             ##lets train the model
             model.fit(train_features, train_labels, eval_metric='mae', eval_set=eval_set, verbose=True)
 
+            ##lets run a feature importance weight analysis
+            self.run_weight_analysis(model)
+
             ##lets plot results
             history = model.evals_result()
 
@@ -148,4 +151,21 @@ class xgb:
 
         return model
 
+    def run_weight_analysis(self, model):
+        feature_importance = model.feature_importances_
+        print("Feature Importances")
+        print(feature_importance)
+        plt.figure(figsize=(10,8))
+        xg.plot_importance(model, importance_type="weight")
+        plt.show()
+
+    def load_model(self, name, inputs, alfa):
+        
+        model = self.build_model((inputs+1)*self.depth, alfa, 1)
+        model_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'checkpoints', 'xgboost'))
+        model_file = os.path.join(model_dir, f'{name}.json')
+        print(f'Path : {model_file}')
+        model.load_model(model_file)
+
+        return model
 
