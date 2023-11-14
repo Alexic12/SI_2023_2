fc1a4d5d7b8653b44ddf2b8cb1f4bfebe57b07b8
Matias Osuna <matias.osuna.e@gmail.com>
2023-11-14 10:34:25 -0500

    Trabajo Final Con Paris

--- control_arduino.py
+++ control_arduino.py
@@ -4,8 +4,6 @@ import keyboard
 import numpy as np
 from hubs.models.xgboost import xgb
 
-control_vector = np.zeros(7)
-previousControl = 0
 
 def read_position(ser):
     # Read the position from the serial port
@@ -17,24 +15,20 @@ def read_position(ser):
         print("Received invalid data:", line)
         return None
 
-def calculate_control_output(model,position, setpoint):
+def calculate_control_output(model,position, setpoint, control_vector, previousControl):
     # Implement your control logic here.
     # For this example, I'm just implementing a dummy control that tries to maintain the position at 50%.
-    error = setpoint - position
-    control_vector[0] = control_vector[1]
-    control_vector[1] = setpoint
-    control_vector[2] = control_vector[3]
-    control_vector[3] = error
-    control_vector[4] = control_vector[5]
-    control_vector[5] = position
-    control_vector[6] = 0
     control_output = inverse_neuronal_control(control_vector, previousControl, model)
-    previousControl = control_output
+    control_output *= 8 #hay que des-normalizar la salida
+    if control_output >= 5:
+        control_output = 5
+    if control_output <= 0:
+        control_output = 0
     return control_output
 
 def inverse_neuronal_control(control_vector, label, model):
 
-        label = label.reshape((1,1))
+        #label = label.reshape((1,1))
         control_vector = control_vector.reshape((1,7))
 
         ##lets create an evaluation set
@@ -45,25 +39,40 @@ def inverse_neuronal_control(control_vector, label, model):
 
 def main():
     # Set up the serial connection
-    ser = serial.Serial('COM3', 115200)  # replace 'COM3' with your port and 9600 with your baud rate
+    ser = serial.Serial(port="COM16", baudrate=115200)  # replace 'COM3' with your port and 9600 with your baud rate
     time.sleep(2)  # give some time for the connection to establish
-    setpoint = 0
+    setpoint = [0,3,5,2,4,1]
+    index = 0
     xg = xgb(10)
     model = xg.load_model(name = 'INDENT_PID_PLANTA',inputs =  7, alfa = 0.02)
+    control_vector = np.zeros(7)
+    previousControl = 0
     try:
         while True:
             position = read_position(ser)
             if position is not None:
                 print(f"Current Position: {position}%")
-                control_output = calculate_control_output(model,position,setpoint)
+                error = setpoint[index] - position
+                control_vector[0] = control_vector[1]
+                control_vector[1] = position
+                control_vector[2] = control_vector[3]
+                control_vector[3] = error
+                control_vector[4] = control_vector[5]
+                control_vector[5] = setpoint[index]
+                control_vector[6] = previousControl
+                control_output = calculate_control_output(model,position,setpoint[index],control_vector,previousControl)
+                previousControl = control_output
+                print(f"Setpoint: {setpoint[index]}")
                 print(f"Sending Control Output: {control_output}")
-                ser.write(str(control_output).encode('utf-8'))  # send the control output
+                ser.write((str(control_output)+'\n').encode('utf-8'))  # send the control output
                 time.sleep(0.03)  # delay for stability, adjust as necessary
             
             if keyboard.is_pressed('t'):
-                input("presione enter para cambiar setpoint")
-                setpoint=int(input("ingresar setpoint entre 1 y 5"))
-    except keyboard.is_pressed('q'):
+                index +=1
+                if index == 6:
+                    index = 0
+                time.sleep(0.1)
+    except KeyboardInterrupt:
         print("Program terminated.")
     finally:
         ser.close()
